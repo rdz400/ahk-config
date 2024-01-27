cdf608b78c2d3e4cc3ad8c5a0b61bc3065023dec
Ronald <dietz.r@outlook.com>
2024-01-27 09:13:15 +0100

    Remove unused python file

--- parse.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    import sys
-    _, passed = sys.argv
-    passed = passed.strip("\n").split(" ")
-    print(",".join(passed))
-    print("And this is an even longer testttttt")
